2f3636b8525a6f8463cc6eea370aeb8da1c0c48b
Duke <dukethanakorn@gmail.com>
2025-03-17 21:54:07 +0700

    Completed 9.1 to 9.7!

--- index.ts
+++ index.ts
@@ -1,14 +1,16 @@
 // index.ts
-import express from 'express';
-import { calculateBmi } from './typescript-exercises/bmiCalculator'; 
+import express, { Request, Response } from 'express';
+import { calculateBmi } from './bmiCalculator';
+import { calculateExercises, ExerciseResult } from './exerciseCalculator';
 
 const app = express();
+app.use(express.json());
 
-app.get('/hello', (_req, res) => {
+app.get('/hello', (_req: Request, res: Response) => {
   res.send('Hello Full Stack!');
 });
 
-app.get('/bmi', (req, res) => {
+app.get('/bmi', (req: Request, res: Response) => {
   const height = Number(req.query.height);
   const weight = Number(req.query.weight);
 
@@ -18,6 +20,31 @@ app.get('/bmi', (req, res) => {
 
   const bmi = calculateBmi(height, weight);
   return res.json({ weight, height, bmi });
+});
+
+interface ExerciseRequestBody {
+  daily_exercises: number[];
+  target: number;
+}
+
+app.post('/exercises', (req: Request, res: Response) => {
+  // Type the request body
+  const { daily_exercises, target } = req.body as ExerciseRequestBody;
+
+  if (!daily_exercises || !target) {
+    return res.status(400).json({ error: 'parameters missing' });
+  }
+
+  
+  const dailyHours: number[] = daily_exercises;
+  const targetValue = Number(target);
+
+  if (isNaN(targetValue) || !Array.isArray(dailyHours) || dailyHours.some(h => isNaN(Number(h)))) {
+    return res.status(400).json({ error: 'malformatted parameters' });
+  }
+
+  const result: ExerciseResult = calculateExercises(dailyHours, targetValue);
+  return res.json(result);
 });
 
 const PORT = 3003;
